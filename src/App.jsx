b52e5473bad8d302d01a0fb4f6f168d81ceb9636
tyto-ovo <sjollyparker@gmail.com>
2024-10-05 14:26:00 -0300

    correccion hito anterior

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
       <Navbar />
       <Register />
-      {/*       <Home /> */}
+      <Home />
       <Footer />
     </>
   );
